712525dae225baed1e9007c4aecffd96b99538f3
= <devleonardo@hotmail.com>
2019-05-24 13:00:11 -0300

    First model-controller-view of Department

--- FirstSystemAsp/Controllers/HomeController.cs
+++ FirstSystemAsp/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using FirstSystemAsp.Models;
+using FirstSystemAsp.Models.ViewModels;
 
 namespace FirstSystemAsp.Controllers {
     public class HomeController : Controller {
--- FirstSystemAsp/Models/ViewModels/ErrorViewModel.cs
+++ FirstSystemAsp/Models/ViewModels/ErrorViewModel.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace FirstSystemAsp.Models {
+namespace FirstSystemAsp.Models.ViewModels {
     public class ErrorViewModel {
         public string RequestId { get; set; }
 
