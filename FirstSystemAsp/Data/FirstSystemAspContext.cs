f89aeed2f381849ec31bd69640bc757eb8024e92
= <devleonardo@hotmail.com>
2019-05-26 13:01:03 -0300

    Adicionado Scaffolded

---
+++ FirstSystemAsp/Data/FirstSystemAspContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstSystemAsp.Models
+{
+    public class FirstSystemAspContext : DbContext
+    {
+        public FirstSystemAspContext (DbContextOptions<FirstSystemAspContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FirstSystemAsp.Models.Department> Department { get; set; }
+    }
+}
--- FirstSystemAsp/Startup.cs
+++ FirstSystemAsp/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using FirstSystemAsp.Models;
 
 namespace FirstSystemAsp {
     public class Startup {
@@ -28,6 +30,9 @@ namespace FirstSystemAsp {
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+    services.AddDbContext<FirstSystemAspContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("FirstSystemAspContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
